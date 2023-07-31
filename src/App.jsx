94c76b0819714bd71377db05d78e74c16ce424ea
Sheryll Dumapal <sheryllhhs@outlook.com>
2023-07-31 17:51:30 +0200

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 import "./App.css";
+import React, { useState } from "react"; //I2
+import HomePage from "./pages/HomePage"; //I2
+import { Route, Routes } from 'react-router-dom' // I2: add the route
+import AllBeersPage from "./pages/AllBeersPage"; 
+import RandomBeer from "./pages/RandomBeerPage";
+import AddBeerPage from "./pages/AddBeerPage"; 
+import BeerDetailsPage from "./pages/BeerDetailsPage"; 
 
-function App() {
+function App () {
+  //const [AllBeers, setAllBeers] = useState([]);   
+  //const [RandomBeers, setRandomBeers] = useState([]);   
+  //const [NewBeer, setNewBeer] = useState([]);   
+  
   return (
-    <div className="App">
-      <h1>LAB | React IronBeers</h1>
-    </div>
+    <>
+    <nav> 
+    <Routes>
+        <Route path="/" element= {<HomePage/>}  /> //I1
+        <Route path="/Allbeers" element= {<AllBeersPage/>}  /> //I1
+        <Route path="/add-beer" element= {<AddBeerPage/>}  /> 
+        <Route path="/random-beer" element= {<RandomBeerPage/>}  /> 
+        <Route path="/beers/:beerId" element= {<BeerDetailsPage/>}  /> 
+        </Routes>
+    </nav>
+    </>
   );
 }
 
 export default App;
+
+
+/*
+*/
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,50 @@
-function HomePage() {}
+//function HomePage() {}
+//import React, { useEffect, useState} from "react"; -you don't need it in the newer versions
+import beersImage from "../assets/beers.png"; 
+import { Link} from "react-router-dom";
 
+function HomePage() {
+    return (
+        <>
+          <div className="HomePageContainer" >
+          <img src={beersImage} alt= "All Beers" />
+          <h1>
+            <Link to="/beers">All Beers</Link>
+          </h1>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <img src={RandomBeer} alt= "Random Beers" />
+          <h1>
+            <Link to="/random-beer">All Beers</Link>
+          </h1>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <img src={NewBeer} alt= "New Beers" />
+          <h1>
+            <Link to="/new-beer">A</Link>
+          </h1>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+            </div>  
+        </>
+    );
+}
 export default HomePage;
+/*
+const HomePage = () => {
+    return (
+        <>
+       // here the link 
+        <img src={`${assets.beers}.png`} />
+     <h1>All Beers</h1>
+     <p> here text? </p>
+        
+        </>
+    )
+}
+*/ 
+
+
